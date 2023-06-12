4a4f017ab04a0611091828fdb3994c2532e3e797
Illes Kornel <kornelilles10@egmail.com>
2023-06-12 14:49:54 +0200

    Implemented Resilience4j to enhance the reliability and fault tolerance of the communication with the Account Service in the Client Service

--- client-service/src/main/java/com/kali/banksystem/clientservice/service/ClientService.java
+++ client-service/src/main/java/com/kali/banksystem/clientservice/service/ClientService.java
@@ -7,15 +7,20 @@ import com.kali.banksystem.clientservice.dto.client.ClientRequest;
 import com.kali.banksystem.clientservice.dto.client.ClientResponse;
 import com.kali.banksystem.clientservice.model.Client;
 import com.kali.banksystem.clientservice.repository.ClientRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
@@ -30,8 +35,11 @@ public class ClientService {
         this.webClientBuilder = webClientBuilder;
     }
 
-
-    public void registerClient(ClientRequest clientRequest) {
+    @CircuitBreaker(name = "account",fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "account")
+    @Retry(name = "account")
+    @Transactional
+    public CompletableFuture<String> registerClient(ClientRequest clientRequest) {
         Client client = Client.builder()
                 .firstName(clientRequest.getFirstName())
                 .lastName(clientRequest.getLastName())
@@ -46,20 +54,25 @@ public class ClientService {
                 .accountIds(new ArrayList<>())
                 .build();
 
+        try {
+            // Save the client entity
+            clientRepository.save(client);
+            log.info("Client {} is registered", client.getId());
 
-        //call the account service and create a base account
-        clientRepository.save(client);
-        log.info("client {} is registered", client.getId());
+            AccountRequest accountRequest = getAccountRequest(client);
+            Mono<AccountResponse> createdAccount = createAccount(accountRequest);
 
-        AccountRequest accountRequest = getAccountRequest(client);
-        Mono<AccountResponse> createdAccount = createAccount(accountRequest);
+            AccountResponse account = createdAccount.block(); // Wait for the Mono to complete and get the result
+            log.info("base account {} is created for client id {} ", account.getId(), client.getId());
+            return  CompletableFuture.supplyAsync(() ->"You register successfully!");
 
-        createdAccount.subscribe(account -> {
-            // Print the account details
-            log.info("Created account: {}", account.getId());
-        });
-
+        } catch (Exception e) {
+            // Handle the exception
+            log.error("Failed to create account for client {}", client.getId(), e);
+            throw e;
+        }
     }
+
 
     private AccountRequest getAccountRequest(Client client) {
         return AccountRequest.builder()
@@ -94,10 +107,21 @@ public class ClientService {
     }
 
     private Mono<AccountResponse> createAccount(AccountRequest accountRequest) {
-        return webClientBuilder.build().post()
-                .uri("http://account-service/api/account")
-                .bodyValue(accountRequest)
-                .retrieve()
-                .bodyToMono(AccountResponse.class);
+        try {
+            return webClientBuilder.build().post()
+                    .uri("http://account-service/api/account")
+                    .bodyValue(accountRequest)
+                    .retrieve()
+                    .bodyToMono(AccountResponse.class);
+        } catch (Exception e) {
+            // Handle or rethrow the exception
+            log.error("Failed to create account", e);
+            throw e;
+        }
     }
+
+    private CompletableFuture<String>  fallBackMethod(ClientRequest clientRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() ->"Oops! Something went wrong, please try later!");
+    }
+
 }
